e03b47502725570cba1333e97d93527f01b27dfd
DESKTOP-3K0QUFJ\DJCOTTO <sergiopereirahidalgo@gmail.com>
2023-03-03 01:46:53 +0100

    Commit V3

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import { home } from './vistas/home'
+import { header } from './componentes/header'
+import { admin } from './vistas/admin'
+import { footer } from './componentes/footer'
+
+/*import { header } from './componentes/header'
+import { footer } from './componentes/footer'
+import { home } from './vistas/home'
+*/
+document.querySelector('header').innerHTML = header.template
+document.querySelector('main').innerHTML = home.template
+
+
+home.script()
+header.script()
+document.querySelector('footer').innerHTML = footer.template
